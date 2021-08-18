0b0a1cdde178be7df59ab945faefa2b6d3a1945f
EthanSystem <ElementsPhoenix@outlook.com>
2021-08-18 18:34:58 +0800

    mod 2021-08-18 18:34:55

--- ZoteroOperation/uploadFiles_client.js
+++ ZoteroOperation/uploadFiles_client.js
@@ -1,11 +1,11 @@
 
-
+// ---------------
+// 程序1：批量导入附件（已弃用）
 // import md file.
 var file_json_title = "./ZoteroOperation/data/goal/mdfilestitlename.json"
 var fs = require('fs')
 var data_json = JSON.parse(fs.readFileSync(file_json_title))
 console.log(data_json)
-
 // Get a item that by search.
 var zoteroSearch = new Zotero.Search();
 zoteroSearch.libraryID = Zotero.Libraries.userLibraryID;
@@ -16,9 +16,6 @@ for (let i in data_json) {
     var item = items[0];
     return item;
 }
-
-
-
 // Get an item's attachments
 var fulltext = [];
 if (item.isRegularItem()) { // not an attachment already
@@ -32,3 +29,87 @@ if (item.isRegularItem()) { // not an attachment already
     }
 }
 return fulltext;
+
+// ---------------
+
+
+
+// ---------------
+// 程序2：修改tag之属性「自动、手动」。其中，item.addTag(tag, num)中，num=0是手动，num=1是自动。
+var old_name = "内容：国际金融危机";
+var num = 1;
+var zotero_search = new Zotero.Search();
+zotero_search.libraryID = ZoteroPane.getSelectedLibraryID();
+zotero_search.addCondition('tag', 'is', old_name);
+var items_id = await zotero_search.search();
+if (!items_id.length) {
+    return "No items found";
+}
+await Zotero.DB.executeTransaction(async function () {
+    for (let id of items_id) {
+        let item = Zotero.Items.get(id);
+        item.addTag(old_name, num);
+        await item.save({
+            skipDateModifiedUpdate: true
+        });
+    }
+});
+return items_id.length + " tag(s) updated";
+
+// ---------------
+
+
+
+
+// ---------------
+// 程序3：修改检索获得的相关item之field之名称（未验证）
+var field_name = 'tag';
+var condition = 'is';
+var old_name = "财政政策";
+var new_name = "内容：财政政策";
+var zotero_search = new Zotero.Search();
+zotero_search.libraryID = ZoteroPane.getSelectedLibraryID();
+zotero_search.addCondition(field_name, condition, old_name);
+var items_id = await zotero_search.search();
+if (!items_id.length) {
+    return "No items found";
+}
+await Zotero.DB.executeTransaction(async function () {
+    for (let item_id of items_id) {
+        let item = Zotero.Items.get(item_id);
+        let mapped_field_id=zot.
+        await item.save({
+            skipDateModifiedUpdate: true
+        });
+    }
+});
+return items_id.length + " tag(s) updated";
+
+// ---------------
+
+
+
+
+// ---------------
+// 程序4：修改选取的相关item之field之名称（未验证）
+var field_name = 'tag';
+var condition = 'is';
+var old_name = "财政政策";
+var new_name = "内容：财政政策";
+var items = Zotero.getActiveZoteroPane().getSelectedItems();
+if (!items.length) {
+    return "No items found";
+}
+// return(items)
+await Zotero.DB.executeTransaction(async function () {
+    for (let item_id of items_id) {
+        let item = Zotero.Items.get(item_id);
+        let mapped_field_id=zot.
+        await item.save({
+            skipDateModifiedUpdate: true
+        });
+    }
+});
+return items_id.length + " tag(s) updated";
+
+// ---------------
